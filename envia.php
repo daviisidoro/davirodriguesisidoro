7a282564fee657b89baa406a880ad298e7930496
Davi Isidoro <daviisidoro99@gmail.com>
2024-10-04 00:13:46 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -1,19 +1,18 @@
 <?php
 
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $telefone = addcslashes($_POST['telefone']);
-    $mensagem = addcslashes($_POST['mensagem']);
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
 
     $para = "daviisidorocontato@gmail.com";
-    $assunto = "Contato" ;
+    $assunto = "Coleta de dados - Inteliogia";
 
-    $corpo = "Nome: " .$nome."\n"."Email :" .$email."\n"."Telefone: " .$telefone."\n"."Mensagem: " .$mensagem;
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone;
 
-    $cabeca = "From: daviisidoro99@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+    $cabeca = "From: srdavi46@gmail.com - daviisidoro"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
 
-    if (mail($para,$assunto,$corpo,$cabeca)){
-        echo("Email enviado com Sucesso!");
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
     }else{
         echo("Houve um erro ao enviar o email!");
     }
